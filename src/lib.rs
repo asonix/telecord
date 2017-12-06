6ecffceb5069280311b51b1d3188114175e540a9
Riley Trautman <asonix@tamu.edu>
2017-12-05 19:06:57 -0600

    Move documentation to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,24 @@
 // You should have received a copy of the GNU General Public License
 // along with Telecord  If not, see <http://www.gnu.org/licenses/>.
 
-//! This is the library for Telebot.
+//! # Telecord: A bot to link Discord channels to Telegram Channels
+//!
+//! Telecord currently sends text messages between Telegram and Discord, and media messages from
+//! Telegram to Discord. Media messages from Discord to Telegram are in the works, but are
+//! currently blocked on `issue#11` for the Telebot crate
+//!
+//! In order to run this crate, a few environment variables must be set
+//! - `DISCORD_BOT_TOKEN` must contain the token for your discord bot
+//! - `TELEGRAM_BOT_TOKEN` must contain the token for your telegram bot
+//! - `CHAT_MAPPINGS` must be a comma-separated list of colon-separated tuples. What this means is
+//!     for each discord channel and telegram chat you wish to connect, you must specify the
+//!     telegram chat's ID and the discord channel's ID in the following format:
+//!     `telegram_chat_id:discord_channel_id`. You can have as many connected chats and channels as
+//!     you would like by adding more to the mapping: `tg_id:dc_id,tg_id2:dc_id2,tg_id3,dc_id3`.
+//!
+//! Once your environment variables are set, you can run the crate with `cargo run`
+//!
+//! ## This is the library for Telebot.
 //!
 //! It may not be incredibly useful outside the context of the associated main.rs
 
--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,21 @@
 // You should have received a copy of the GNU General Public License
 // along with Telecord  If not, see <http://www.gnu.org/licenses/>.
 
-/// # Telecord: A bot to link Discord channels to Telegram Channels
-///
-/// Telecord currently sends text messages between Telegram and Discord, and media messages from
-/// Telegram to Discord. Media messages from Discord to Telegram are in the works, but are
-/// currently blocked on `issue#11` for the Telebot crate
-///
-/// In order to run this crate, a few environment variables must be set
-/// - `DISCORD_BOT_TOKEN` must contain the token for your discord bot
-/// - `TELEGRAM_BOT_TOKEN` must contain the token for your telegram bot
-/// - `CHAT_MAPPINGS` must be a comma-separated list of colon-separated tuples. What this means is
-///     for each discord channel and telegram chat you wish to connect, you must specify the
-///     telegram chat's ID and the discord channel's ID in the following format:
-///     `telegram_chat_id:discord_channel_id`. You can have as many connected chats and channels as
-///     you would like by adding more to the mapping: `tg_id:dc_id,tg_id2:dc_id2,tg_id3,dc_id3`.
-///
-/// Once your environment variables are set, you can run the crate with `cargo run`
-///
-/// ### Main.rs
-///
-/// Telecord relies on the Serenity library to interface with Discord, and the Telebot library to
-/// interface with Telegram. These libraries have fundementally different architectures, so mapping
-/// from one to the other requires a few processes.
-///
-/// Serentiy relies on a threadpool to handle blocking work across multiple cores, while Telebot
-/// relies on Tokio's event-loop to handle nonblocking work on a single thread. Main.rs reflects
-/// this in its flow.
-///
-/// First, we generate our Config struct, and create senders and receivers for the messages passed
-/// between Serenity and Telebot, then we create a thread to handle messages forwarded to discord.
-/// Next, we create a thread to enclose the Tokio event-loop, which handles both receiving messages
-/// from Telegram and sending them to the Discord thread, and receiving messages from Serenity and
-/// sending them to Telegram. Finally, we start Serenity's threadpool from the current thread.
+// Main.rs
+//
+// Telecord relies on the Serenity library to interface with Discord, and the Telebot library to
+// interface with Telegram. These libraries have fundementally different architectures, so mapping
+// from one to the other requires a few processes.
+//
+// Serentiy relies on a threadpool to handle blocking work across multiple cores, while Telebot
+// relies on Tokio's event-loop to handle nonblocking work on a single thread. Main.rs reflects
+// this in its flow.
+//
+// First, we generate our Config struct, and create senders and receivers for the messages passed
+// between Serenity and Telebot, then we create a thread to handle messages forwarded to discord.
+// Next, we create a thread to enclose the Tokio event-loop, which handles both receiving messages
+// from Telegram and sending them to the Discord thread, and receiving messages from Serenity and
+// sending them to Telegram. Finally, we start Serenity's threadpool from the current thread.
 
 extern crate telebot;
 extern crate tokio_core;
